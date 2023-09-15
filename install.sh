e3382c101be71ba5564fc3722da6af500af9b5fe
bdloser404 <126883200+bdloser404@users.noreply.github.com>
2023-09-15 21:31:50 +0600

    Update install.sh
    Adding Sh -c feature.

--- install.sh
+++ install.sh
@@ -1,21 +1,34 @@
 #!/data/data/com.termux/files/usr/bin/bash
-#!/bin/bash
+
 trap 'exit_script' SIGINT
-function exit_script() {
-  read -n 1 -t 5 -p "Do you want to exit? (Y/n) " choice
-  choice="${choice,,}"  # Convert the input to lowercase
-  if [[ "$choice" == "y" ]]; then
-    exit 1
-  fi
+exit_script() {
+  read -n 1 -t 5 -p "Exit? (Y/n) " choice
+  choice="${choice,,}"
+  [ "$choice" == "y" ] && exit 1
 }
+
 apt update && apt upgrade -y
 apt install ncurses-utils openssl-tool nodejs -y
-
 pkg install neovim -y
 
 install(){
-	chmod +x fluttermux
-	cp fluttermux $PREFIX/bin/
-	fluttermux --install
+  fluttermux --install
 }
-install
+
+if [ -f "$PREFIX/bin/fluttermux" ]; then
+  rm "$PREFIX/bin/fluttermux"
+fi
+
+FLUTTERMUX_URL="https://raw.githubusercontent.com/bdloser404/Fluttermux/main/fluttermux"
+
+if curl -Lo "$PREFIX/bin/fluttermux" "$FLUTTERMUX_URL"; then
+  chmod +x "$PREFIX/bin/fluttermux"
+  # Run the install function
+  install
+else
+  tput bold
+  echo "Download failed. Check the URL and try again."
+  tpur sgr0
+  exit 1
+fi
+
